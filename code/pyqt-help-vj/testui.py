27548358f2a819140578d08c89525dd31dd8cb53
Joshua Fitzmaurice <jfitz02@outlook.com>
2023-01-18 18:22:09 +0000

    Got top topics screen working

--- code/pyqt-help-vj/testui.py
+++ code/pyqt-help-vj/testui.py
@@ -2,26 +2,63 @@ import sys
 from PyQt5 import QtWidgets, uic, QtGui
 
 from twitter_API import data_collator
-from twitter_API import model_api
+from twitter_API.data_collator import filename_creation
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 import os
 import re
+import sqlite3 as sql
 
-twitter_processor = model_api.TweetProcessor(os.getcwd(), [])
-tweet_collator = data_collator.DataCollator("./twitter_API/pass.secret")
+id2label = {
+    0: "medicine",
+    1: "business",
+    2: "videogames",
+    3: "education",
+    4: "religion",
+    5: "science",
+    6: "philosophy",
+    7: "politics",
+    8: "music",
+    9: "sports",
+    10: "law",
+    11: "culture",
+    12: "economics",
+    13: "geography",
+    14: "technology",
+    15: "mathematics",
+    16: "history",
+    17: "foods",
+    18: "disasters",
+    19: "entertainment"
+}
+
+tweet_collator = data_collator.DataCollator("./twitter_API/pass.secret", id2label, )
 
 Ui_findTopic, _ = uic.loadUiType("findtopic.ui")
 Ui_topTopics, _ = uic.loadUiType("toptopics.ui")
 Ui_wordCloud, _ = uic.loadUiType("wordcloud.ui")
 
+def generate_wordcloud(text):
+    # Create and generate a word cloud image:
+    filename = filename_creation(text, "wordcloud")
+    wordcloud = WordCloud(width=335, height=370).generate(text)
+    print(os.getcwd())
+    # save the wordcloud
+    wordcloud.to_file(filename)
+
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.twitter_tweetset = tweet_collator.get_twitter_tweets()
+        self.your_tweetset = tweet_collator.get_tweets()
+
         self.stacked_widget = QtWidgets.QStackedWidget()
         self.wordCloud()
         self.topTopics()
         self.findTopics()
         self.setCentralWidget(self.stacked_widget)
         self.resize(800,600)
+
 
     def wordCloud(self):
         self.wordCloud_widget = QtWidgets.QMainWindow()
@@ -34,26 +71,32 @@ class Ui(QtWidgets.QMainWindow):
         self.get_your_wordcloud()
         self.get_twitter_wordcloud()
 
-    def get_your_wordcloud(self):
-        # get tweets
-        tweets = tweet_collator.get_tweets()
+    def gen_wordcloud(self, tweetset):
+        tweets = tweet_collator.db_get_tweets(tweetset)
         text = ""
         for tweet in tweets:
-            text += tweet.full_text + " "
+            text += tweet[0]
 
         # remove links, and "RT"
         text = re.sub(r"http\S+", "", text)
         text = re.sub(r"RT", "", text)
 
-        twitter_processor.generate_wordcloud(text)
+        generate_wordcloud(text)
 
+        return text
+
+
+    def get_your_wordcloud(self):
+        text = self.gen_wordcloud(self.your_tweetset)
         # get wordcloud.png and load into graphicsView
         self.graphicsView = self.wordCloud_widget.findChild(QtWidgets.QGraphicsView, 'graphicsView')
         self.scene = QtWidgets.QGraphicsScene()
 
         self.graphicsView.setScene(self.scene)
+
+        filename = filename_creation(text, "wordcloud")
         
-        self.image_qt = QtGui.QImage(f"media_store/wordclouds/{text[-10:]}.png")
+        self.image_qt = QtGui.QImage(filename)
 
         pic = QtWidgets.QGraphicsPixmapItem()
         pic.setPixmap(QtGui.QPixmap.fromImage(self.image_qt))
@@ -61,26 +104,17 @@ class Ui(QtWidgets.QMainWindow):
         self.scene.addItem(pic)
 
     def get_twitter_wordcloud(self):
-        # get tweets
-        tweets = tweet_collator.get_twitter_tweets()
-        text = ""
-        for tweet in tweets:
-            text += tweet.full_text
-
-        # remove links, and "RT"
-        text = re.sub(r"http\S+", "", text)
-        text = re.sub(r"RT", "", text)
-
-
-        twitter_processor.generate_wordcloud(text)
+        text =self.gen_wordcloud(self.twitter_tweetset)
 
         # get wordcloud.png and load into graphicsView
         self.graphicsView = self.wordCloud_widget.findChild(QtWidgets.QGraphicsView, 'graphicsView_2')
         self.scene = QtWidgets.QGraphicsScene()
 
         self.graphicsView.setScene(self.scene)
+
+        filename = filename_creation(text, "wordcloud")
         
-        self.image_qt = QtGui.QImage(f"media_store/wordclouds/{text[-10:]}.png")
+        self.image_qt = QtGui.QImage(filename)
 
         pic = QtWidgets.QGraphicsPixmapItem()
         pic.setPixmap(QtGui.QPixmap.fromImage(self.image_qt))
@@ -96,6 +130,31 @@ class Ui(QtWidgets.QMainWindow):
         self.button1.clicked.connect(lambda : self.goto_page(self.wordCloud_widget))
         self.button2 = self.topTopics_widget.findChild(QtWidgets.QPushButton, 'pushButton_2')
         self.button2.clicked.connect(lambda : self.goto_page(self.findTopics_widget))
+        self.get_toptopics()
+        self.get_toptopics(yours=False)
+
+    def get_toptopics(self, yours=True):
+        # get tweets
+        if yours:
+            lbl = "yl"
+            prog = "yprog"
+            top_topics = tweet_collator.db_get_top_topics(self.your_tweetset)
+        else:
+            lbl = "tl"
+            prog = "tprog"
+            top_topics = tweet_collator.db_get_top_topics(self.twitter_tweetset)
+        #generate probabilities for each topic
+        total = sum(top_topics.values())
+        for key in top_topics:
+            top_topics[key] = top_topics[key]/total
+            
+        # filter to only grab top 5 keys based on value
+        top_topics = dict(sorted(top_topics.items(), key=lambda item: item[1], reverse=True)[:5])
+        for i, (key, value) in enumerate(top_topics.items()):
+            self.topTopics_widget.findChild(QtWidgets.QLabel, f"{lbl}{i+1}").setText(key)
+            print(f"value ] {int(value*100)}")
+            self.progress = self.topTopics_widget.findChild(QtWidgets.QProgressBar, f"{prog}{i+1}").setValue(int(value*100))
+            
 
     def findTopics(self):
         self.findTopics_widget = QtWidgets.QMainWindow()
--- code/pyqt-help-vj/twitter_API/data_collator.py
+++ code/pyqt-help-vj/twitter_API/data_collator.py
@@ -1,4 +1,103 @@
 import tweepy
+import sqlite3 as sql
+import numpy as np
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Wav2Vec2ForCTC, Wav2Vec2Tokenizer, pipeline
+import torch
+import requests
+import librosa
+import moviepy.editor as mp
+from keras_ocr.detection import Detector
+from keras_ocr.pipeline import Pipeline
+from keras_ocr.recognition import Recognizer
+from keras_ocr.tools import read as kread
+import re
+
+def filename_creation(text, type):
+        #remove whitespace
+        text = re.sub(r"\W+", "", text)
+
+        #remove special characters
+        text = re.sub(r"[^a-zA-Z0-9]+", "", text)
+        if type == "wordcloud":
+            return f"media_store/wordclouds/{text[-10:]}.png"
+        elif type == "video":
+            return f"media_store/mp4/{text[-25:-15]}.mp4"
+        elif type == "audio":
+            return f"media_store/wav/{text[-25:-15]}.wav"
+        elif type == "image":
+            return f"media_store/jpg/{text[-15:-5]}.jpg"
+
+class TweetProcessor:
+    def __init__(self, labels):
+        self.labels = labels
+        self.topic_tokenizer = AutoTokenizer.from_pretrained("MrFitzmaurice/roberta-finetuned-topic-5")
+        self.topic_model = AutoModelForSequenceClassification.from_pretrained("MrFitzmaurice/roberta-finetuned-topic-5")
+        self.wav2vec2_tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+        self.wav2vec2_model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+        self.pipeline = Pipeline(detector=Detector(), recognizer=Recognizer())
+
+    def _process_image(self, image_url):
+        # get image
+        r = requests.get(image_url)
+        filename = filename_creation(image_url, "image")
+        with open(filename, "wb") as f:
+            f.write(r.content)
+
+        # get test image
+        image = kread(filename)
+        preds = self.pipeline.recognize(images=[image])[0]
+        words = [pred for pred, _ in preds]
+        # #turn into string
+        words = " ".join(words)
+
+        return words
+
+    def _process_audio(self, audio):
+        r = requests.get(audio)
+        vfilename = filename_creation(audio, "video")
+        afilename = filename_creation(audio, "audio")
+        with open(vfilename, "wb") as f:
+            f.write(r.content)
+
+        clip = mp.VideoFileClip(vfilename)
+        try:
+            clip.audio.write_audiofile(afilename)
+        except AttributeError as e:
+            print("No sound in video")
+            return ""
+
+        audio, _ = librosa.load(afilename, sr=16000)
+        input_values = self.wav2vec2_tokenizer(audio, return_tensors="pt").input_values
+        logits = self.wav2vec2_model(input_values).logits
+        predicted_ids = torch.argmax(logits, dim=-1)
+        transcription = self.wav2vec2_tokenizer.batch_decode(predicted_ids)[0]
+
+        return transcription
+
+    def _roberta_call(self, text):
+        input_ids = self.topic_tokenizer(text, return_tensors="pt")
+        output = self.topic_model(**input_ids)
+        pred = output.logits[0].detach().numpy()
+
+        # convert to probabilities
+        pred = np.exp(pred) / np.sum(np.exp(pred), axis=0)
+
+        return pred
+
+    def predict(self, text, image_url=None, audio_url=None):
+        words = ""
+        if image_url:
+            words += self._process_image(image_url)
+        if audio_url:
+            words += self._process_audio(audio_url)
+
+        words += text
+
+        if len(words) > 512:
+            words = words[:512]
+        pred = self._roberta_call(words)
+        return pred
+
 
 class TestStreaming(tweepy.StreamingClient):
     def __init__(self, bearer_token, twitterfname):
@@ -35,11 +134,56 @@ class TestStreaming(tweepy.StreamingClient):
         print("Error: {}".format(status_code))
 
 class DataCollator:
-    def __init__(self, secrets:str):
+    def __init__(self, secrets:str, labels):
+        self.labels = labels
         self.tokens = self.get_tokens(secrets)
         self.client = self.get_client()
         streaming_client = TestStreaming(self.tokens["bearer_token"], "tweets.txt")
         streaming_client.filter()
+
+        self.database_connect()
+        self.database_setup()
+
+        self.processor = TweetProcessor(labels)
+
+    def database_connect(self):
+        self.conn = sql.connect("twitter.db")
+        self.c = self.conn.cursor()
+
+    def database_setup(self):
+        self.c.execute("""CREATE TABLE IF NOT EXISTS tweetset (
+            tweetset_id INTEGER PRIMARY KEY,
+            medicine_val FLOAT,
+            business_val FLOAT,
+            videogames_val FLOAT,
+            education_val FLOAT,
+            religion_val FLOAT,
+            science_val FLOAT,
+            philosophy_val FLOAT,
+            politics_val FLOAT,
+            music_val FLOAT,
+            sports_val FLOAT,
+            law_val FLOAT,
+            culture_val FLOAT,
+            economics_val FLOAT,
+            geography_val FLOAT,
+            technology_val FLOAT,
+            mathematics_val FLOAT,
+            history_val FLOAT,
+            foods_val FLOAT,
+            disasters_val FLOAT,
+            entertainment_val FLOAT
+        )""")
+
+        self.c.execute("""CREATE TABLE IF NOT EXISTS tweets (
+            tweet_id INTEGER PRIMARY KEY,
+            tweetset_id INTEGER,
+            tweet_text TEXT,
+            tweet_link TEXT,
+            tweet_media_link TEXT,
+            tweet_topic TEXT,
+            FOREIGN KEY (tweetset_id) REFERENCES tweetset (tweetset_id)
+        )""")
 
     def get_tokens(self, filename:str):
         token_dict = {}
@@ -48,7 +192,6 @@ class DataCollator:
                 parts = line.split("=")
                 token_dict[parts[0].strip()] = parts[1].strip()
 
-        print(token_dict)
         return token_dict
 
     def get_client(self):
@@ -61,7 +204,46 @@ class DataCollator:
 
     def get_tweets(self):
         tweets = self.client.home_timeline(tweet_mode="extended", count=50)
-        return tweets
+        #sum together the prediction vectors for each tweet
+        predictions = np.zeros(20)
+        tweet_info = []
+        for tweet in tweets:
+            pred = self.processor.predict(tweet.full_text)
+            tweet_info.append((tweet, self.labels[np.argmax(pred)]))
+            predictions += pred
+
+        tweetset_id = self.db_create_tweetset(predictions)
+        for tweet in tweet_info:
+            self.db_create_tweet(tweetset_id, tweet[0], tweet[1])
+
+        return tweetset_id
+
+    def db_create_tweetset(self, preds):
+        self.c.execute("INSERT INTO tweetset VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", list(preds))
+        self.conn.commit()
+        return self.c.lastrowid
+
+    def db_create_tweet(self, tweetset_id, tweet, topic):
+        url = self.find_media_url(tweet)
+        if url is not None:
+            self.c.execute("INSERT INTO tweets VALUES (NULL, ?, ?, ?, ?, ?)", (tweetset_id, tweet.full_text, "https://twitter.com/twitter/statuses/"+str(tweet.id), url[1], topic))
+        else:
+            self.c.execute("INSERT INTO tweets VALUES (NULL, ?, ?, ?, ?, ?)", (tweetset_id, tweet.full_text, "https://twitter.com/twitter/statuses/"+str(tweet.id), None, topic))
+        
+        self.conn.commit()
+
+    def db_get_top_topics(self, tweetset_id):
+        self.c.execute("SELECT * FROM tweetset WHERE tweetset_id=?", (tweetset_id,))
+        # create dict of topic:val
+        data = self.c.fetchall()[0]
+        topics = {}
+        for i in range(len(self.labels)):
+            topics[self.labels[i]] = data[i+1]
+        return topics
+
+    def db_get_tweets(self, tweetset_id):
+        self.c.execute("SELECT tweet_text FROM tweets WHERE tweetset_id=?", (tweetset_id,))
+        return self.c.fetchall()
 
     def get_twitter_tweets(self):
         tweets = []
@@ -75,7 +257,21 @@ class DataCollator:
                     print(f"{tweet_id} not found")
                     pass
 
-        return tweets
+        predictions = np.zeros(20)
+        tweet_info = []
+        for tweet in tweets:
+            pred = self.processor.predict(tweet.full_text)
+            tweet_info.append((tweet, self.labels[np.argmax(pred)]))
+            predictions += pred
+
+        # divide by number of tweets to get average
+        predictions /= len(tweets)
+
+        tweetset_id = self.db_create_tweetset(predictions)
+        for tweet in tweet_info:
+            self.db_create_tweet(tweetset_id, tweet[0], tweet[1])
+
+        return tweetset_id
 
     def get_tweet(self, tweet_id:int):
         tweet = self.client.get_status(tweet_id, tweet_mode="extended")

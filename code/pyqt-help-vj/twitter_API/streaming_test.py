09f8140c3eb38e76edf9dc802813bef2837806e9
Joshua Fitzmaurice <jfitz02@outlook.com>
2023-02-03 15:48:26 +0000

    created mock API due to twitter API becoming pay to use

--- code/pyqt-help-vj/twitter_API/streaming_test.py
+++
@@ -1,9 +0,0 @@
-from data_collator import DataCollator
-
-dc = DataCollator("pass.secret")
-
-tweets = dc.get_twitter_tweets()
-print(tweets[0].full_text)
-
-mytweets = dc.get_tweets()
-print(mytweets[0].full_text)
